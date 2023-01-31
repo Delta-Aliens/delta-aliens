61e59af2fec30b9963eeaf839e8d44e63da2e3f5
Harry <harryzhu45@gmail.com>
2023-01-30 19:12:17 -0800

    Add and rename scene

--- Delta Aliens/Assets/_Scripts/PlayerController.cs
+++ Delta Aliens/Assets/_Scripts/PlayerController.cs
@@ -4,36 +4,36 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	public CharacterController controller;
-	public float runSpeed = 40f;
-	float horizontalMove = 0f;
-	bool jump = false;
-	bool crouch = false;
-	
-	// Update is called once per frame
-	void Update () {
+    public CharacterController controller;
+    public float runSpeed = 40f;
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
+    
+    // Update is called once per frame
+    void Update () {
 
-		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
-		if (Input.GetButtonDown("Jump"))
-		{
-			jump = true;
-		}
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
 
-		if (Input.GetButtonDown("Crouch"))
-		{
-			crouch = true;
-		} else if (Input.GetButtonUp("Crouch"))
-		{
-			crouch = false;
-		}
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        } else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
 
-	}
+    }
 
-	void FixedUpdate ()
-	{
-		// Move our character
-		controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
-		jump = false;
-	}
+    void FixedUpdate ()
+    {
+        // Move our character
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
 }
