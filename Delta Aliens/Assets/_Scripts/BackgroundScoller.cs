77e5663fba1c8c717a134072776625849338a86c
alexc <amills20@georgefox.edu>
2023-03-13 20:57:54 -0700

    background mostly working now

--- Delta Aliens/Assets/_Scripts/BackgroundScoller.cs
+++ Delta Aliens/Assets/_Scripts/BackgroundScoller.cs
@@ -3,9 +3,12 @@ using UnityEngine;
 public class BackgroundScoller : MonoBehaviour
 {
     [Range(-1f,1f)]
-    public float scollSpeed = 0.5f;
+    public float scrollSpeed;
     private float offset;
     private Material mat;
+    // reference to the player
+    public PlayerController Player;
+
 
     void Start()
     {
@@ -14,7 +17,8 @@ public class BackgroundScoller : MonoBehaviour
 
     void Update()
     {
-        offset += (Time.deltaTime * scollSpeed) / 10f;
+        offset += (Time.deltaTime * scrollSpeed) / 10f;
         mat.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+        scrollSpeed = Player.horizontalMove * 0.065f;
     }
 }
